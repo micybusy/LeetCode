3cb5df5b755ec3ee9490c9c376166e35debbed27
Harun Lokman Kılıç <harunlokmank@gmail.com>
2022-12-07 00:14:49 +0300

    one solution and one problem.

---
+++ last_word_len.py
@@ -0,0 +1,20 @@
+# recursive & elegant solution
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        x = s.split(" ").pop()
+        return len(x) if x else self.lengthOfLastWord(s[:-1])
+
+
+# fast solution
+'''class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        x = s.split(" ")#.pop()
+        for i in range(len(x)):
+            if x[-i-1]:
+                return len(x[-i-1])'''
+
+
+if __name__ == '__main__':
+    fn = Solution().lengthOfLastWord
+    x = "   fly me   to   the moon  "
+    print(fn(x))
